cdb20e35612301abacc83a997263e2ef4040165b
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2009-10-12 18:57:05 +0000

    Further fixes for py2exe.

--- branches/new_object/Volatility/setup.py
+++ branches/new_object/Volatility/setup.py
@@ -3,12 +3,21 @@
 from distutils.core import setup
 import volatility
 import sys
+import os
+import glob
 
 py2exe_available = True
 try:
     import py2exe #pylint: disable-msg=W0611,F0401
 except ImportError:
     py2exe_available = False
+
+def find_py_files(topdirs):
+    ret = []
+    for topdir in topdirs:
+        for r, ds, fs in os.walk(topdir):
+            ret.append((r, [ os.path.join(r, f) for f in fs if f.endswith('.py')]))
+    return ret
 
 opts = {}
 
@@ -21,13 +30,9 @@ opts['url']          = "http://www.volatilesystems.com"
 opts['license']      = "GPL"
 opts['scripts']      = ["volatility.py"]
 opts['packages']     = ["volatility",
-                        "volatility.win32",
-                        "memory_plugins",
-                        "memory_plugins.internal",
-                        "memory_plugins.registry",
-                        "memory_objects",
-                        "memory_objects.Windows",
-                        ]
+                        "volatility.win32"]
+opts['data_files']   = find_py_files(['memory_plugins',
+                                      'memory_objects'])
 
 if py2exe_available:
     py2exe_distdir = 'dist/py2exe'
@@ -36,7 +41,7 @@ if py2exe_available:
                       }]
     opts['options'] = {'py2exe':{'optimize': 2,
                                  'dist_dir': py2exe_distdir,
-                                 'packages': opts['packages'],
+                                 'packages': opts['packages'] + ['socket', 'ctypes', 'Crypto.Cipher'],
                                 }
                       }
 
