afd5e05ce8a7c41908b1c40916d9235fc896e5dc
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2009-10-29 19:53:10 +0000

    Convert fileparam to a callback.
    This ensures that file parameters are accepted at any time once all the
    options are parsed.

--- branches/experimental/plugins/fileparam.py
+++ branches/experimental/plugins/fileparam.py
@@ -12,26 +12,12 @@ import volatility.addrspace as addrspace #pylint: disable-msg=W0611
 
 config = conf.ConfObject()
 
-config.add_option("FILENAME", default = None,
-                  short_option = 'f',
+def set_location(_option, _opt_str, value, parser):
+    """Sets the location variable in the parser to the filename in question"""
+    if parser.values.location == None:
+        parser.values.location = urlparse.urlunparse(('file', '', os.path.abspath(value), '', '', ''))
+
+config.add_option("FILENAME", default = None, action="callback",
+                  callback = set_location, type='str',
+                  short_option = 'f', nargs = 1,
                   help = "Filename to use when opening an image")
-
-## Check if that has been set - we parse all options just in
-## case. Note that it might be possible for options to be set after
-## invalid options and we wont see them here. For example say --foobar
-## is defined in a plugin which has not been loaded yet, and the argvs
-## look like:
-
-## volatility.py --foobar --filename XXXX
-
-## We will not see filename here because parsing will stop until the
-## foobar has been defined.
-
-## The proper solution to this is to come up with an Event system like
-## in PyFlag where events can be emitted at different points in the
-## startup process.
-config.parse_options(final=False)
-
-if config.FILENAME and not config.LOCATION:
-    config.update('LOCATION', urlparse.urlunparse(
-        ('file', '', os.path.abspath(config.FILENAME), '', '', '')))
