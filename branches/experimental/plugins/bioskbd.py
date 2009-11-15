154422438f1fdf084040171ee8eb93d61656f98e
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2009-11-15 16:09:52 +0000

    Fix up GPL headers and attribution.
    I've attempted to include the proper attribution from code taken from
    earlier versions of volatility.  If I've made any mistakes (additions or
    omissions), I'm sorry and do please correct them.

--- branches/experimental/plugins/bioskbd.py
+++ branches/experimental/plugins/bioskbd.py
@@ -1,10 +1,24 @@
-'''
-Created on 3 Oct 2009
-
-@author: Mike Auty
-
-*Heavily* based upon http://www.storm.net.nz/static/files/bioskbsnarf
-'''
+# Volatility
+#
+# Authors:
+# Adam Boileau <metlstorm@storm.net.nz>
+# Mike Auty <mike.auty@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+#
+# *Heavily* based upon http://www.storm.net.nz/static/files/bioskbsnarf
 
 import struct
 import volatility.commands as commands
