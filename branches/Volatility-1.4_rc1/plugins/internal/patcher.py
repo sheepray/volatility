db4857978ca169eacbfaf1607337a27bd3ed6a05
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2010-08-09 16:09:09 +0000

    Attempt to simply patcher plugin, whilst adding speed calibration.

--- branches/Volatility-1.4_rc1/plugins/internal/patcher.py
+++ branches/Volatility-1.4_rc1/plugins/internal/patcher.py
@@ -19,13 +19,13 @@
 #
 
 import sys
+import timeit
 import binascii
 import xml.etree.cElementTree as etree
 import volatility.commands as commands
 import volatility.utils as utils
 import volatility.conf as conf
 config = conf.ConfObject()
-
 PAGESIZE = 4096
 
 #XML Example file format
@@ -36,7 +36,9 @@ PAGESIZE = 4096
 #      <match offset="[offset within page]">DEADBEEFC0FFEE</match>
 #      ...
 #    </constraints>
-#    <patch offset="[offset within page]">BEEFF00DEE</match>
+#    <patches>
+#      <patch offset="[offset within page]">BEEFF00DEE</match>
+#    </patches>
 #    ...
 #  </patchinfo>
 #</patchfile>
@@ -46,65 +48,85 @@ class MultiPageScanner(object):
     
        Designed to minimize reads/writes to the address space
     """
-    def __init__(self, full=True):
-        self.constraints = {}
+    def __init__(self, patchers, full=False):
+        self.patchers = list(patchers)
         self.maxlen = 0
         self.remove_patchers = not full
-        self.patcher_set = set()
+    
+    def use_fullpage(self, address_space):
+        """Calibrate the scanner to ensure fastest speed"""    
+        # Define the calibration functions
+        timeit_fullpage = lambda: list(self.scan_page(address_space, 0, True))
+        timeit_nonfullpage = lambda: list(self.scan_page(address_space, 0, False))
         
-    def add_constraint(self, patcher, offset, data):
-        """Adds a single constraint from a particular patch instance"""
-        checklist = self.constraints.get(offset, [])
-        checklist.append((patcher, data))
-        # So we can tell when we have all the patchers
-        self.patcher_set.add(patcher)
-        # So we know the maximum we have to read
-        self.maxlen = max(self.maxlen, len(data))
-        self.constraints[offset] = checklist
+        with_fullpage = timeit.repeat(timeit_fullpage, number = 100)
+        without_fullpage = timeit.repeat(timeit_nonfullpage, number = 100)
+        return min(with_fullpage) < min(without_fullpage)
 
     def scan(self, address_space, outfd):
         """Scans through the pages"""
         page_offset = 0
 
-        while address_space.is_valid_address(page_offset + PAGESIZE):
+        sys.stdout.write("Calibrating for speed: ")
+        sys.stdout.flush()
+        fullpage = self.use_fullpage(address_space)
+        if fullpage:
+            sys.stdout.write("Reading full pages\n")
+        else:
+            sys.stdout.write("Reading patch locations per page\n")
+        sys.stdout.flush()
+
+        done = False
+        while address_space.is_valid_address(page_offset + PAGESIZE) and not done:
             sys.stdout.write("\rScanning: {0:08X}".format(page_offset))
             sys.stdout.flush()
-            # Keep a list so we can ignore checks in the future...
-            failed_patchers = set()
-            for offset in sorted(self.constraints.keys()):
-                for patcher, data in self.constraints[offset]:
-                    if patcher not in failed_patchers:
-                        testdata = address_space.read(page_offset + offset, len(data))
-                        if data != testdata:
-                            failed_patchers.add(patcher)
-                            
+                        
             # Run through any patchers that didn't fail
-            for patcher in self.patcher_set:
-                if patcher not in failed_patchers:
-                    outfd.write("\rPatching {0} at page {1:x}\n".format(patcher.get_name(), page_offset))
-                    patcher.patch(address_space, page_offset)
-                    if self.remove_patchers:
-                        self.patcher_set.remove(patcher)
-                        self.remove_patcher(patcher)
+            for patcher in self.scan_page(address_space, page_offset, fullpage):
+                outfd.write("\rPatching {0} at page {1:x}\n".format(patcher.get_name(), page_offset))
+                patcher.patch(address_space, page_offset)
+                if self.remove_patchers:
+                    self.patchers.remove(patcher)
+                    # Stop if we've got nothing left to look for
+                    if not len(self.patchers):
+                        done = True
+                        
             # Jump to the next page
             page_offset += PAGESIZE
         sys.stdout.write("\n")
 
-    def remove_patcher(self, patcher):
-        """Removes a patcher from the constraints structure"""
-        for o in self.constraints:
-            self.constraints[o] = [(p, d) for (p, d) in self.constraints[o] if p != patcher] 
+    def scan_page(self, address_space, page_offset, fullpage = False):
+        """Runs through patchers for a single page"""
+        if fullpage:
+            pagedata = address_space.read(page_offset, PAGESIZE)
+
+        for patcher in self.patchers:
+            for offset, data in patcher.get_constraints():
+                if fullpage:
+                    testdata = pagedata[offset:offset + len(data)]
+                else:
+                    testdata = address_space.read(page_offset + offset, len(data))
+                if data != testdata:
+                    break
+            else:
+                yield patcher
 
 class Patcher(object):
     """Simple object to hold patching data"""
     def __init__(self, name):
-        self.patches = []
         self.name = name
+        self.patches = set()
+        self.constraints = set()
+
+    def add_constraint(self, offset, data):
+        """Adds a constraint to the constraintlist"""            
+        # Ensure that all offsets are within PAGESIZE
+        self.constraints.add((offset % PAGESIZE, data))
 
     def add_patch(self, offset, patch):
         """Adds a patch to the patchlist"""
         # Ensure that all offsets are within PAGESIZE
-        self.patches.append((offset % PAGESIZE, patch))
+        self.patches.add((offset % PAGESIZE, patch))
         
     def patch(self, addr_space, page_offset):
         """Writes to the address space"""
@@ -116,6 +138,9 @@ class Patcher(object):
     def get_patches(self):
         """Returns the list of patches for this patcher"""
         return self.patches
+
+    def get_constraints(self):
+        return self.constraints
 
     def get_name(self):
         """Returns the name of the patcher"""
@@ -133,7 +158,7 @@ class dllpatch(commands.command):
     def calculate(self):
         """Calculates the patchers"""
         addr_space = utils.load_as(astype='physical')
-        scanner = self.parse_patchfile()
+        scanner = MultiPageScanner(self.parse_patchfile())
         return scanner, addr_space
     
     def render_text(self, outfd, data):
@@ -152,53 +177,46 @@ class dllpatch(commands.command):
             base = 16
         return int(offset, base)        
 
-    def make_patcher(self, element, scanner):
-        """Adds a patchinfo to the scanner object"""
-        if not config.WRITE:
-            config.error("The patcher plugin requires that write support be enabled")
-        patcher = Patcher(element.get('name', 'Unlabelled'))
-        constraints = None
-        for tag in element:
-            if tag.tag == 'constraints':
-                constraints = tag
-            if tag.tag == 'patches':
-                patches = tag
-        if constraints is None:
-            config.error("Patch input file does not contain any valid constraints")
-
-        # Parse the patches section
-        for tag in patches:
-            if tag.tag == 'setbytes':
-                offset = self.get_offset(tag)
-                data = binascii.a2b_hex(tag.text)
-                if offset is not None and len(data):
-                    patcher.add_patch(offset, data)
-        if not len(patcher.get_patches()):
-            # No patches, no point adding this
-            return False
-        
-        # Parse the constraints section
-        for c in constraints:
-            if c.tag == 'match':
-                offset = self.get_offset(c)
-                data = binascii.a2b_hex(c.text)
-                if offset is not None and len(data):
-                    scanner.add_constraint(patcher, offset, data)
-        return True
-
     def parse_patchfile(self):
         """Parses the patch XML data"""
-        scanner = MultiPageScanner(True)
         if config.XML_INPUT is None:
             config.error("No XML input file was specified")
         try:
             root = etree.parse(config.XML_INPUT).getroot()
         except SyntaxError, e:
             config.error("XML input file was improperly formed: " + str(e))
-        for patchinfo in root:
-            if patchinfo.tag == 'patchinfo':
-                if patchinfo.get('method', 'nomethod') == 'pagescan':
-                    self.make_patcher(patchinfo, scanner)
+
+        for element in root:
+            if element.tag == 'patchinfo':
+                if element.get('method', 'nomethod') == 'pagescan':
+                    patcher = Patcher(element.get('name', 'Unlabelled'))
+                    constraints = None
+                    for tag in element:
+                        if tag.tag == 'constraints':
+                            constraints = tag
+                        if tag.tag == 'patches':
+                            patches = tag
+                    if constraints is None:
+                        config.error("Patch input file does not contain any valid constraints")
+            
+                    # Parse the patches section
+                    for tag in patches:
+                        if tag.tag == 'setbytes':
+                            offset = self.get_offset(tag)
+                            data = binascii.a2b_hex(tag.text)
+                            if offset is not None and len(data):
+                                patcher.add_patch(offset, data)
+                    if not len(patcher.get_patches()):
+                        # No patches, no point adding this
+                        break
+                    
+                    # Parse the constraints section
+                    for c in constraints:
+                        if c.tag == 'match':
+                            offset = self.get_offset(c)
+                            data = binascii.a2b_hex(c.text)
+                            if offset is not None and len(data):
+                                patcher.add_constraint(offset, data)
+                    yield patcher
                 else:
-                    config.error("Unsupported patchinfo method " + patchinfo.method)
-        return scanner
+                    config.error("Unsupported patchinfo method " + element.method)
