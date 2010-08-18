c014996e9fa8e88c7eae4a13b33176582fde9ab6
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2010-08-18 00:34:12 +0000

    Ensure that debug operations are no-ops without a -d to avoid confusing users.

--- branches/Volatility-1.4_rc1/volatility/debug.py
+++ branches/Volatility-1.4_rc1/volatility/debug.py
@@ -35,11 +35,13 @@ def debug(msg, level = 1):
 
 def b():
     """Enters the debugger at the call point"""
-    pdb.set_trace()
+    if config.DEBUG:
+        pdb.set_trace()
 
 def trace():
     """Enters the debugger at the call point"""
-    pdb.set_trace()
+    if config.DEBUG:
+        pdb.set_trace()
 
 def post_mortem():
     """Provides a command line interface to python after an exception's occurred"""
