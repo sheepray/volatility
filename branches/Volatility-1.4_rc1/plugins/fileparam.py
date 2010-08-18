1d72f42bd71965035a7a599157e083339e35b8e2
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2010-08-18 00:31:16 +0000

    Add in fix for pathname2url on windows, solves Issue 2.

--- branches/Volatility-1.4_rc1/plugins/fileparam.py
+++ branches/Volatility-1.4_rc1/plugins/fileparam.py
@@ -20,6 +20,7 @@
 
 import volatility.conf as conf
 import urllib
+import sys
 import os
 ## This is required to ensure that LOCATION is defined here
 import volatility.addrspace as addrspace #pylint: disable-msg=W0611
@@ -29,7 +30,12 @@ config = conf.ConfObject()
 def set_location(_option, _opt_str, value, parser):
     """Sets the location variable in the parser to the filename in question"""
     if parser.values.location == None:
-        parser.values.location = "file://" + urllib.pathname2url(os.path.abspath(value))
+        slashes = "//"
+        # Windows pathname2url decides to convert C:\blah to ///C:/blah
+        # So to keep the URLs correct, we only add file: rather than file://
+        if sys.platform.startswith('win'):
+            slashes = ""
+        parser.values.location = "file:" + slashes + urllib.pathname2url(os.path.abspath(value))
 
 config.add_option("FILENAME", default = None, action = "callback",
                   callback = set_location, type = 'str',
