a26e801b0685770815bf3d70d872e61b1bfbcde8
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2010-09-02 11:36:38 +0000

    Make it explicit that the scan buffer's profile is copied from the address_space.  Helps issue 26.

--- branches/Volatility-1.4_rc1/volatility/scan.py
+++ branches/Volatility-1.4_rc1/volatility/scan.py
@@ -86,6 +86,7 @@ class BaseScanner(object):
 
     overlap = 20
     def scan(self, address_space, offset = 0, maxlen = None):
+        self.buffer.profile = address_space.profile
         self.base_offset = offset
         self.max_length = maxlen
         ## Which checks also have skippers?
