6bd8f6f60f5ce440f7c4a8e8b80a17f0cc45d0a3
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2010-09-26 22:15:18 +0000

    Ensure that pickled objects have the correct module names, by reimporting the files once we know their namespaces.

--- branches/Volatility-1.4_rc1/volatility/registry.py
+++ branches/Volatility-1.4_rc1/volatility/registry.py
@@ -39,7 +39,7 @@ functionality. For example we may include a Scanner, Report and File
 classes in the same plugin and have them all automatically loaded.
 """
 
-import os, sys, imp
+import os, sys, imp, warnings
 import volatility.conf as conf
 config = conf.ConfObject()
 import volatility.debug as debug #pylint: disable-msg=W0611
@@ -88,38 +88,40 @@ class ModuleRegistry(object):
           The modules which did not load.
         """
 
-        results = []
-        for module_name, module_path in modules:
-            try:
-                ## Temporarily load this module into a temporary
-                ## name. It will be moved later to its desired
-                ## namespace
+        # Ignore the RuntimeWarnings telling us about no lower nodes during absolute import attempts
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", RuntimeWarning)
+            results = []
+            for module_name, module_path in modules:
                 try:
-                    del sys.modules['tmp_module']
-                except KeyError:
-                    pass
+                    ## Temporarily load this module into a temporary
+                    ## name. It will be moved later to its desired
+                    ## namespace
+                    try:
+                        del sys.modules['tmp_module']
+                    except KeyError:
+                        pass
 
-                module = imp.load_source("tmp_module", module_path)
+                    module = imp.load_source("tmp_module", module_path)
 
-                # The module name we use depends on the __namespace__ arg
-                try:
-                    module_name = "{0}.{1}".format(module.__namespace__, module_name)
-                except AttributeError:
-                    pass
+                    # The module name we use depends on the __namespace__ arg
+                    try:
+                        module_name = "{0}.{1}".format(module.__namespace__, module_name)
+                    except AttributeError:
+                        pass
 
-                module.__name__ = module_name
-                modname = "volatility.plugins.{0}".format(module_name)
-                if self.modules.get(modname, None):
-                    config.error("Module " + module_path + " requested an already registered name (" + modname + ")")
-                self.insert_module_to_system(modname, module)
+                    module_name = "volatility.plugins.{0}".format(module_name)
+                    if self.modules.get(module_name, None):
+                        config.error("Module " + module_path + " requested an already registered name (" + module_name + ")")
+                    self.insert_module_to_system(module_name, module_path)
 
-            except ImportError, e:
-                results.append((module_name, module_path))
-                self.errors[module_path] = e
+                except ImportError, e:
+                    results.append((module_name, module_path))
+                    self.errors[module_path] = e
 
         return results
 
-    def insert_module_to_system(self, module_name, module):
+    def insert_module_to_system(self, module_name, module_path):
         """Inserts the module into the sys.modules dict ensuring that
         intermediate modules are created.
 
@@ -132,6 +134,10 @@ class ModuleRegistry(object):
         ## We basically add the module to the module tree by ensure it
         ## has a continuous path to the root. If a node is missing we
         ## add a dummy node.
+
+        # We have to reload the source to ensure the objects get created with the right module name
+        # so that pickling it later all fits together properly.
+        module = imp.load_source(module_name, module_path)
         self.modules[module_name] = sys.modules[module_name] = module
 
         ## Now check that its reachable
