9d18c45e093bf8d094ebfa5abb92c34ab5e11163
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2010-11-11 22:28:52 +0000

    Big Plugin Move: Ensures core plugins are always loaded, that they're installed to site-packages, that volatility.exe contains a complete program (no external files required), and allows pluins to be loaded directly from zipfiles.

--- branches/Volatility-1.4_rc1/setup.py
+++ branches/Volatility-1.4_rc1/setup.py
@@ -54,15 +54,21 @@ opts['url'] = "http://www.volatilesystems.com"
 opts['license'] = "GPL"
 opts['scripts'] = ["volatility.py"]
 opts['packages'] = ["volatility",
-                        "volatility.win32"]
-opts['data_files'] = find_py_files(['plugins'])
+                    "volatility.win32",
+                    "volatility.plugins",
+                    "volatility.plugins.addrspaces",
+                    "volatility.plugins.overlays",
+                    "volatility.plugins.registry"]
+opts['data_files'] = find_py_files(['contrib'])
 
 if py2exe_available:
     py2exe_distdir = 'dist/py2exe'
     opts['console'] = [{ 'script': 'volatility.py',
 #                          'icon_resources': [(1, 'resources/py.ico')]
                       }]
-    opts['options'] = {'py2exe':{'optimize': 2,
+    # Optimize must be 1 for plugins that use docstring for the help value,
+    # otherwise the help gets optimized out
+    opts['options'] = {'py2exe':{'optimize': 1,
                                  'dist_dir': py2exe_distdir,
                                  'packages': opts['packages'] + ['socket', 'ctypes', 'Crypto.Cipher', 'urllib'],
                                  # This, along with zipfile = None, ensures a single binary
--- branches/Volatility-1.4_rc1/volatility/plugins/__init__.py
+++ branches/Volatility-1.4_rc1/volatility/plugins/__init__.py
@@ -6,4 +6,5 @@ config = conf.ConfObject()
 # This causes the config.PLUGINS paths to be treated as extensions of the volatility.plugins package
 # Meaning that each directory is search for module when import volatility.plugins.module is requested
 
-__path__ = [ os.path.abspath(x) for x in config.PLUGINS.split(";") ]
+if config.PLUGINS:
+    __path__.extend([ os.path.abspath(x) for x in config.PLUGINS.split(";")])
