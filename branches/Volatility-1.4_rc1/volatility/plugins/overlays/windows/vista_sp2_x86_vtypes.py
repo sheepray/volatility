2b969bbe9045a7d043a0432e33481d4af50741e4
mike.auty <mike.auty@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2011-02-15 19:52:40 +0000

    Remove the licensing on automatically generated files to keep everything consistent.  They're still covered by COPYING anyway.

--- branches/Volatility-1.4_rc1/volatility/plugins/overlays/windows/vista_sp2_x86_vtypes.py
+++ branches/Volatility-1.4_rc1/volatility/plugins/overlays/windows/vista_sp2_x86_vtypes.py
@@ -1,22 +1,3 @@
-# Volatility
-# Copyright (c) 2008 Volatile Systems
-# Copyright (c) 2008-2011 Volatile Systems
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at
-# your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details. 
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
-#
-
 ntkrnlmp_types = {
   '_KRESOURCEMANAGER_COMPLETION_BINDING' : [ 0x14, {
     'NotificationListHead' : [ 0x0, ['_LIST_ENTRY']],
