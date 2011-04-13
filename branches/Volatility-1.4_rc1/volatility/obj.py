57b210ee1afb1fa97936caa87b2ad3b0d511fffb
mooyix <mooyix@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2011-04-13 19:25:20 +0000

    __or__ was left out of NumericProxyMixIn, which was causing hibinfo not to work

--- branches/Volatility-1.4_rc1/volatility/obj.py
+++ branches/Volatility-1.4_rc1/volatility/obj.py
@@ -439,7 +439,7 @@ class NumericProxyMixIn(object):
     _specials = [
         ## Number protocols
         '__add__', '__sub__', '__mul__', '__floordiv__', '__mod__', '__divmod__',
-        '__pow__', '__lshift__', '__rshift__', '__and__', '__xor__', '__div__',
+        '__pow__', '__lshift__', '__rshift__', '__and__', '__xor__', '__or__', '__div__',
         '__truediv__', '__radd__', '__rsub__', '__rmul__', '__rdiv__', '__rtruediv__',
         '__rfloordiv__', '__rmod__', '__rdivmod__', '__rpow__', '__rlshift__',
         '__rrshift__', '__rand__', '__rxor__', '__ror__', '__neg__', '__pos__',
