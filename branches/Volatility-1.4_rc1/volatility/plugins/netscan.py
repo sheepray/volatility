4028f311937a29a34b904b67f126bdc5a66d0db2
michael.hale@gmail.com <michael.hale@gmail.com@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2011-04-14 20:44:35 +0000

    Always use in-house inet_ntop instead of socket.inet_ntop. Fixes Issue #106.

--- branches/Volatility-1.4_rc1/volatility/plugins/netscan.py
+++ branches/Volatility-1.4_rc1/volatility/plugins/netscan.py
@@ -24,6 +24,7 @@ import volatility.scan as scan
 import volatility.obj as obj
 import volatility.cache as cache
 import socket
+import itertools
 
 tcp_states = [
     "", # This must be empty, so the first enum starts at 1
@@ -45,60 +46,57 @@ tcp_states = [
 AF_INET = 2
 AF_INET6 = 0x17
 
-# Compensate for Windows python not supporting socket.inet_ntop
-try:
-    inet_ntop = socket.inet_ntop
-except AttributeError:
-    import itertools
+# Compensate for Windows python not supporting socket.inet_ntop and some
+# Linux systems (i.e. OpenSuSE 11.2 w/ Python 2.6) not supporting IPv6. 
 
-    def inet_ntop(address_family, packed_ip):
+def inet_ntop(address_family, packed_ip):
 
-        def inet_ntop4(packed_ip):
-            if not isinstance(packed_ip, str):
-                raise TypeError("must be string, not {0}".format(type(packed_ip)))
-            if len(packed_ip) != 4:
-                raise ValueError("invalid length of packed IP address string")
-            return "{0}.{1}.{2}.{3}".format(*[ord(x) for x in packed_ip])
+    def inet_ntop4(packed_ip):
+        if not isinstance(packed_ip, str):
+            raise TypeError("must be string, not {0}".format(type(packed_ip)))
+        if len(packed_ip) != 4:
+            raise ValueError("invalid length of packed IP address string")
+        return "{0}.{1}.{2}.{3}".format(*[ord(x) for x in packed_ip])
 
-        def inet_ntop6(packed_ip):
-            if not isinstance(packed_ip, str):
-                raise TypeError("must be string, not {0}".format(type(packed_ip)))
-            if len(packed_ip) != 16:
-                raise ValueError("invalid length of packed IP address string")
+    def inet_ntop6(packed_ip):
+        if not isinstance(packed_ip, str):
+            raise TypeError("must be string, not {0}".format(type(packed_ip)))
+        if len(packed_ip) != 16:
+            raise ValueError("invalid length of packed IP address string")
 
-            words = []
-            for i in range(0, 16, 2):
-                words.append((ord(packed_ip[i]) << 8) | ord(packed_ip[i + 1]))
+        words = []
+        for i in range(0, 16, 2):
+            words.append((ord(packed_ip[i]) << 8) | ord(packed_ip[i + 1]))
 
-            # Replace a run of 0x00s with None
-            numlen = [(k, len(list(g))) for k, g in itertools.groupby(words)]
-            max_zero_run = sorted(sorted(numlen, key = lambda x: x[1], reverse = True), key = lambda x: x[0])[0]
-            words = []
-            for k, l in numlen:
-                if (k == 0) and (l == max_zero_run[1]) and not (None in words):
-                    words.append(None)
-                else:
-                    for i in range(l):
-                        words.append(k)
+        # Replace a run of 0x00s with None
+        numlen = [(k, len(list(g))) for k, g in itertools.groupby(words)]
+        max_zero_run = sorted(sorted(numlen, key = lambda x: x[1], reverse = True), key = lambda x: x[0])[0]
+        words = []
+        for k, l in numlen:
+            if (k == 0) and (l == max_zero_run[1]) and not (None in words):
+                words.append(None)
+            else:
+                for i in range(l):
+                    words.append(k)
 
-            # Handle encapsulated IPv4 addresses
-            encapsulated = ""
-            if (words[0] is None) and (len(words) == 3 or (len(words) == 4 and words[1] == 0xffff)):
-                words = words[:-2]
-                encapsulated = inet_ntop4(packed_ip[-4:])
-            # If we start or end with None, then add an additional :
-            if words[0] is None:
-                words = [None] + words
-            if words[-1] is None:
-                words += [None]
-            # Join up everything we've got using :s
-            return ":".join(["{0:x}".format(w) if w is not None else "" for w in words]) + encapsulated
+        # Handle encapsulated IPv4 addresses
+        encapsulated = ""
+        if (words[0] is None) and (len(words) == 3 or (len(words) == 4 and words[1] == 0xffff)):
+            words = words[:-2]
+            encapsulated = inet_ntop4(packed_ip[-4:])
+        # If we start or end with None, then add an additional :
+        if words[0] is None:
+            words = [None] + words
+        if words[-1] is None:
+            words += [None]
+        # Join up everything we've got using :s
+        return ":".join(["{0:x}".format(w) if w is not None else "" for w in words]) + encapsulated
 
-        if address_family == socket.AF_INET:
-            return inet_ntop4(packed_ip)
-        elif address_family == socket.AF_INET6:
-            return inet_ntop6(packed_ip)
-        raise socket.error("[Errno 97] Address family not supported by protocol")
+    if address_family == socket.AF_INET:
+        return inet_ntop4(packed_ip)
+    elif address_family == socket.AF_INET6:
+        return inet_ntop6(packed_ip)
+    raise socket.error("[Errno 97] Address family not supported by protocol")
 
 # String representations of INADDR_ANY and INADDR6_ANY
 inaddr_any = inet_ntop(socket.AF_INET, '\0' * 4)
