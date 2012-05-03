c91a7211b66772adb690b93ed2c6c92295625256
mike.auty@gmail.com <mike.auty@gmail.com@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2012-05-03 22:19:17 +0000

    Add in mount linux plugin, comment out most of the common code until its required.

--- branches/linux-trunk/volatility/plugins/linux/linux_common.py
+++ branches/linux-trunk/volatility/plugins/linux/linux_common.py
@@ -24,6 +24,9 @@
 import volatility.commands as commands
 import volatility.utils    as utils
 import volatility.obj      as obj
+import volatility.debug    as debug
+
+MAX_STRING_LENGTH = 256
 
 
 def mask_number(num):
@@ -42,122 +45,76 @@ class AbstractLinuxCommand(commands.Command):
     def is_valid_profile(profile):
         return profile.metadata.get('os', 'Unknown').lower() == 'linux'
 
-
-def sizeofstruct(struct_name, profile):
-
-    return profile.get_obj_size(struct_name)
-
-def offsetof(struct_name, list_member, profile):
-
-    offset = profile.get_obj_offset(struct_name, list_member)
-    return offset
-
-def bit_is_set(bmap, pos):
-
-    mask = 1 << pos
-    return bmap & mask
-
-# returns a list of online cpus (the processor numbers)
-def online_cpus(smap, addr_space):
-
-    #later kernels..
-    if "cpu_online_bits" in smap:
-        bmap = obj.Object("unsigned long", offset = smap["cpu_online_bits"], vm = addr_space)
-
-    elif "cpu_present_map" in smap:
-        bmap = obj.Object("unsigned long", offset = smap["cpu_present_map"], vm = addr_space)
-
-    else:
-        raise AttributeError, "Unable to determine number of online CPUs for memory capture"
-
-    cpus = []
-    for i in xrange(0, 8):
-        if bit_is_set(bmap, i):
-            cpus.append(i)
-
-    return cpus
-
-def walk_per_cpu_var(obj_ref, per_var, var_type):
-
-    cpus = online_cpus(obj_ref.smap, obj_ref.addr_space)
-
-    # get the highest numbered cpu
-    max_cpu = cpus[-1]
-
-    per_offsets = obj.Object(theType = 'Array', targetType = 'unsigned long', count = max_cpu, offset = obj_ref.smap["__per_cpu_offset"], vm = obj_ref.addr_space)
-    i = 0
-
-    for i in cpus:
-
-        offset = per_offsets[i]
-
-        addr = obj_ref.smap["per_cpu__" + per_var] + offset.v()
-        var = obj.Object(var_type, offset = addr, vm = obj_ref.addr_space)
-
-        yield i, var
-
-
+#def bit_is_set(bmap, pos):
+#
+#    mask = 1 << pos
+#    return bmap & mask
+#
+## returns a list of online cpus (the processor numbers)
+#def online_cpus(smap, addr_space):
+#
+#    # TODO: Consider using bitmask
+#    #later kernels..
+#    if "cpu_online_bits" in smap:
+#        bmap = obj.Object("unsigned long", offset = smap["cpu_online_bits"], vm = addr_space)
+#
+#    elif "cpu_present_map" in smap:
+#        bmap = obj.Object("unsigned long", offset = smap["cpu_present_map"], vm = addr_space)
+#
+#    else:
+#        raise AttributeError, "Unable to determine number of online CPUs for memory capture"
+#
+#    cpus = []
+#    for i in xrange(0, 8):
+#        if bit_is_set(bmap, i):
+#            cpus.append(i)
+#
+#    return cpus
+#
+#def walk_per_cpu_var(obj_ref, per_var, var_type):
+#
+#    cpus = online_cpus(obj_ref.smap, obj_ref.addr_space)
+#
+#    # get the highest numbered cpu
+#    max_cpu = cpus[-1]
+#
+#    per_offsets = obj.Object(theType = 'Array', targetType = 'unsigned long', count = max_cpu, offset = obj_ref.smap["__per_cpu_offset"], vm = obj_ref.addr_space)
+#    i = 0
+#
+#    for i in cpus:
+#
+#        offset = per_offsets[i]
+#
+#        addr = obj_ref.smap["per_cpu__" + per_var] + offset.v()
+#        var = obj.Object(var_type, offset = addr, vm = obj_ref.addr_space)
+#
+#        yield i, var
+#
+#
 
 # similar to for_each_process for this usage
 def walk_list_head(struct_name, list_member, list_head_ptr, addr_space):
+    debug.warning("Deprecated use of walk_list_head")
 
-    list_ptr = list_head_ptr.next
-    offset = offsetof(struct_name, list_member, addr_space.profile)
+    for item in list_head_ptr:
+        yield item
 
-    # this happens in rare instances where list_heads get pre-initlized
-    # the caller needs to check for not return value
-    # currently only needed by linux_mount when walking mount_hashtable
-    if list_ptr == list_head_ptr or not list_ptr:
-        return
+#
+#def walk_internal_list(struct_name, list_member, list_start, addr_space):
+#
+#    # FIXME: Get rid of the while 1 statements
+#    while 1:
+#
+#        list_struct = obj.Object(struct_name, vm = addr_space, offset = list_start)
+#
+#        yield list_struct
+#
+#        list_start = list_struct.__getattribute__(list_member)
+#
+#        if not list_start:
+#            break
+#
 
-    # FIXME: Get rid of the while 1 statements
-    while 1:
-
-        # return the address of the beginning of the strucutre, similar to list.h in kernel
-        yield obj.Object(struct_name, offset = list_ptr - offset, vm = addr_space)
-
-        list_ptr = list_ptr.next
-
-        if list_ptr == list_head_ptr or not list_ptr:
-            break
-
-
-def walk_internal_list(struct_name, list_member, list_start, addr_space):
-
-    # FIXME: Get rid of the while 1 statements
-    while 1:
-
-        list_struct = obj.Object(struct_name, vm = addr_space, offset = list_start)
-
-        yield list_struct
-
-        list_start = list_struct.__getattribute__(list_member)
-
-        if not list_start:
-            break
-
-def get_string(addr, addr_space, maxlen = 256):
-
-    name = addr_space.read(addr, maxlen)
-    ret = ""
-
-    for n in name:
-        if ord(n) == 0:
-            break
-        ret = ret + n
-
-    return ret
-
-
-def format_path(path_list):
-
-    path = '/'.join(path_list)
-
-    return path
-
-def IS_ROOT(dentry):
-
-    return dentry == dentry.d_parent
 
 # based on __d_path
 # TODO: (deleted) support
@@ -166,9 +123,10 @@ def do_get_path(rdentry, rmnt, dentry, vfsmnt, addr_space):
 
     inode = dentry.d_inode
 
+    # TODO: Put a proper guard condition in place of while 1
     while 1:
 
-        dname = get_string(dentry.d_name.name, addr_space)
+        dname = dentry.d_name.name.dereference_as("String", length = MAX_STRING_LENGTH)
 
         if dname != '/':
             ret_path.append(dname)
@@ -176,7 +134,7 @@ def do_get_path(rdentry, rmnt, dentry, vfsmnt, addr_space):
         if dentry == rdentry and vfsmnt == rmnt:
             break
 
-        if dentry == vfsmnt.mnt_root or IS_ROOT(dentry):
+        if dentry == vfsmnt.mnt_root or dentry == dentry.d_parent:
             if vfsmnt.mnt_parent == vfsmnt:
                 break
             dentry = vfsmnt.mnt_mountpoint
@@ -189,7 +147,7 @@ def do_get_path(rdentry, rmnt, dentry, vfsmnt, addr_space):
 
     ret_path.reverse()
 
-    ret_val = format_path(ret_path)
+    ret_val = '/'.join([str(p) for p in ret_path])
 
     if ret_val.startswith(("socket:", "pipe:")):
         if ret_val.find("]") == -1:
@@ -202,37 +160,38 @@ def do_get_path(rdentry, rmnt, dentry, vfsmnt, addr_space):
 
     return ret_val
 
-def get_path(task, filp, addr_space):
-    rdentry = task.fs.get_root_dentry()
-    rmnt = task.fs.get_root_mnt()
-    dentry = filp.get_dentry()
-    vfsmnt = filp.get_vfsmnt()
-
-    return do_get_path(rdentry, rmnt, dentry, vfsmnt, addr_space)
-
-# this is here b/c python is retarded and its inet_ntoa can't handle integers...
-def ip2str(ip):
-
-    a = ip & 0xff
-    b = (ip >> 8) & 0xff
-    c = (ip >> 16) & 0xff
-    d = (ip >> 24) & 0xff
-
-    return "{0}.{1}.{2}.{3}".format(a, b, c, d)
-
-def ip62str(in6addr):
-
-    ret = ""
-    ipbytes = in6addr.in6_u.u6_addr8
-    ctr = 0
-
-    for byte in ipbytes:
-        ret = ret + "{0:02x}".format(byte)
-
-        # make it the : notation
-        if ctr % 2 and ctr != 15:
-            ret = ret + ":"
-
-        ctr = ctr + 1
-
-    return ret
+#
+#def get_path(task, filp, addr_space):
+#    rdentry = task.fs.get_root_dentry()
+#    rmnt = task.fs.get_root_mnt()
+#    dentry = filp.get_dentry()
+#    vfsmnt = filp.get_vfsmnt()
+#
+#    return do_get_path(rdentry, rmnt, dentry, vfsmnt, addr_space)
+#
+## this is here b/c python is retarded and its inet_ntoa can't handle integers...
+#def ip2str(ip):
+#
+#    a = ip & 0xff
+#    b = (ip >> 8) & 0xff
+#    c = (ip >> 16) & 0xff
+#    d = (ip >> 24) & 0xff
+#
+#    return "{0}.{1}.{2}.{3}".format(a, b, c, d)
+#
+#def ip62str(in6addr):
+#
+#    ret = ""
+#    ipbytes = in6addr.in6_u.u6_addr8
+#    ctr = 0
+#
+#    for byte in ipbytes:
+#        ret = ret + "{0:02x}".format(byte)
+#
+#        # make it the : notation
+#        if ctr % 2 and ctr != 15:
+#            ret = ret + ":"
+#
+#        ctr = ctr + 1
+#
+#    return ret
