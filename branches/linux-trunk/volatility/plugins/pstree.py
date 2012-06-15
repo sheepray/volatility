14dc12e307011ea635d458f2759a3791152c39bd
mike.auty@gmail.com <mike.auty@gmail.com@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2012-06-15 01:05:02 +0000

    Merge branch 'local/trunk' into local/linux-trunk

--- branches/linux-trunk/volatility/plugins/pstree.py
+++ branches/linux-trunk/volatility/plugins/pstree.py
@@ -24,18 +24,21 @@ import volatility.win32.tasks as tasks
 import volatility.utils as utils
 import volatility.plugins.common as common
 import volatility.cache as cache
+import volatility.obj as obj
 
 #pylint: disable-msg=C0111
 
-pslist_types = {
-    '_SE_AUDIT_PROCESS_CREATION_INFO' : [ 0x4, {
-    'ImageFileName' : [ 0x0, ['pointer', ['_OBJECT_NAME_INFORMATION']]],
-    } ],
-
-    '_OBJECT_NAME_INFORMATION' : [ 0x8, {
-    'Name' : [ 0x0, ['_UNICODE_STRING']],
-    } ],
-    }
+class ProcessAuditVTypes(obj.ProfileModification):
+    before = ["WindowsVTypes"]
+    conditions = {'os': lambda x: x == 'windows'}
+    def modification(self, profile):
+        profile.vtypes.update({
+            '_SE_AUDIT_PROCESS_CREATION_INFO' : [ 0x4, {
+            'ImageFileName' : [ 0x0, ['pointer', ['_OBJECT_NAME_INFORMATION']]],
+            }],
+            '_OBJECT_NAME_INFORMATION' : [ 0x8, {
+            'Name' : [ 0x0, ['_UNICODE_STRING']],
+            }]})
 
 class PSTree(common.AbstractWindowsCommand):
     """Print process list as a tree"""
@@ -51,25 +54,28 @@ class PSTree(common.AbstractWindowsCommand):
         return pid
 
     def render_text(self, outfd, data):
-        max_pad = 10
-        outfd.write("{0:20}             {1} {2:6} {3:6} {4:6} {5:6} {6:6}\n".format(
-            'Name', " " * max_pad, 'Pid', 'PPid', 'Thds', 'Hnds', 'Time'))
+
+        self.table_header(outfd, 
+                         [("Name", "<50"), 
+                          ("Pid", ">6"),
+                          ("PPid", ">6"),
+                          ("Thds", ">6"),
+                          ("Hnds", ">6"),
+                          ("Time", "20")])
 
         def draw_branch(pad, inherited_from):
             for task, task_info in data.items():
                 if task_info['inherited_from'] == inherited_from:
-                    outfd.write("{0} 0x{1:08X}:{2:20} {3} {4:6} {5:6} {6:6} {7:6} {8:26}\n".format(
-                        "." * pad,
-                        task_info['eprocess'].obj_offset,
-                        task_info['image_file_name'],
-                        " " * (max_pad - pad),
+
+                    self.table_row(outfd, 
+                        "{0} {1:#x}:{2:20}".format("." * pad, task_info['eprocess'].obj_offset, task_info['image_file_name']),
                         task_info['process_id'],
                         task_info['inherited_from'],
                         task_info['active_threads'],
                         task_info['handle_count'],
-                        task_info['create_time']))
+                        task_info['create_time'])
 
-                    if self._config.VERBOSE > 1:
+                    if self._config.VERBOSE:
                         try:
                             outfd.write("{0}    cmd: {1}\n".format(
                                 ' ' * pad, task_info['command_line']))
@@ -80,8 +86,8 @@ class PSTree(common.AbstractWindowsCommand):
                         except KeyError:
                             pass
 
-                    draw_branch(pad + 1, task_info['process_id'])
                     del data[task]
+                    draw_branch(pad + 1, task_info['process_id']) 
 
         while len(data.keys()) > 0:
             keys = data.keys()
@@ -94,7 +100,6 @@ class PSTree(common.AbstractWindowsCommand):
 
         ## Load a new address space
         addr_space = utils.load_as(self._config)
-        addr_space.profile.add_types(pslist_types)
 
         for task in tasks.pslist(addr_space):
             task_info = {}
@@ -109,7 +114,7 @@ class PSTree(common.AbstractWindowsCommand):
             ## Get the Process Environment Block - Note that _EPROCESS
             ## will automatically switch to process address space by
             ## itself.
-            if self._config.VERBOSE > 1:
+            if self._config.VERBOSE:
                 peb = task.Peb
                 if peb:
                     task_info['command_line'] = peb.ProcessParameters.CommandLine
