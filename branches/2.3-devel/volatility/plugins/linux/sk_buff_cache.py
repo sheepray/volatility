0039b855fd38b67ec3a752db8dde2415cd3ae83c
mike.auty@gmail.com <mike.auty@gmail.com@8d5d6628-2090-11de-9909-f37ff7dbbc12>
2012-09-12 07:07:44 +0000

    Merge branch 'local/trunk' into local/2.3-devel

--- branches/2.3-devel/volatility/plugins/linux/sk_buff_cache.py
+++ branches/2.3-devel/volatility/plugins/linux/sk_buff_cache.py
@@ -22,13 +22,13 @@
 """
 
 import os
-
+import volatility.debug as debug
 import volatility.obj as obj
 import volatility.plugins.linux.common as linux_common
 from volatility.plugins.linux.slab_info import linux_slabinfo
 
 class linux_sk_buff_cache(linux_common.AbstractLinuxCommand):
-    """Gather files from the dentry cache"""
+    """Recovers packets from the sk_buff kmem_cache"""
     
     def __init__(self, config, *args): 
         linux_common.AbstractLinuxCommand.__init__(self, config, *args)
@@ -45,7 +45,7 @@ class linux_sk_buff_cache(linux_common.AbstractLinuxCommand):
             data  = self.addr_space.zread(start, pkt_len)
 
             fname = "{0:x}".format(s.obj_offset)
-            fd    = open(os.path.join(self.edir, fname), "wb")
+            fd = open(os.path.join(self.edir, fname), "wb")
             fd.write(data)
             fd.close()
 
@@ -53,7 +53,7 @@ class linux_sk_buff_cache(linux_common.AbstractLinuxCommand):
 
     def walk_cache(self, cache_name):
 
-        cache = linux_slabinfo(self._config).get_kmem_cache(cache_name, self._config.UNALLOCATED, struct_name="sk_buff")
+        cache = linux_slabinfo(self._config).get_kmem_cache(cache_name, self._config.UNALLOCATED, struct_name = "sk_buff")
 
         if not cache:
             return
